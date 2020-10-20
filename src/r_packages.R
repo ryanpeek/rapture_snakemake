b615fdfe491b5388215b3ed54cbd34094ef85155
Ryan Peek <rapeek@ucdavis.edu>
2020-10-20 15:41:10 -0700

    add r pkg list

---
+++ src/r_packages.R
@@ -0,0 +1,12 @@
+# packages
+
+library(conflicted)
+library(tidyverse)
+library(purrr)
+library(glue)
+library(here)
+library(fs)
+library(readxl)
+library(janitor)
+library(knitr)
+conflict_prefer("filter", "dplyr")
